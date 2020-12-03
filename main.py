ca76aeb8dd718f35ee12025ec64e50a1fa669d20
RE-mazked <58600817+RE-mazked@users.noreply.github.com>
2020-12-03 22:26:27 +0100

    first commit
    v0.1

---
+++ main.py
@@ -0,0 +1,26 @@
+def checkPrimary(num):
+    res = 0
+    divBy = 0
+    primaryBool = False
+    primaryBooltemp = False
+    for i in range(0, num):
+        i += 1  
+        res_han = num/i
+        if int(res_han) == res_han and res_han != 1:
+            if primaryBooltemp:
+                primaryBool = True
+                divBy = i
+                break
+            primaryBooltemp = True
+        else:
+            res += res_han
+    return primaryBool, divBy, res
+
+
+if __name__ == '__main__':
+    num = int(input("Write a number:"))
+    primaryBool, divBy, res = checkPrimary(num)
+    if primaryBool:
+        print(f'Not prime primary, divisible by {divBy}')
+    else:
+        print('Prime')
